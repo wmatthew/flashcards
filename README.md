4f37dc11c88db3da492f64827fc9daead2272e9d
Matthew Webber <Matthew.Webber@gmail.com>
2016-04-10 10:01:14 -0700

    initial checkin w Smith family

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # flashcards
-flashcard generator
+
+This is a flashcard generator for helping remember large groups of people.
+
+This takes in a CSV file listing personal details (names, nicknames, birthdays) and an SVG template and generates a PNG file for each person.
+
+This was designed to help memorize the members of an actual family. I've .gitignore'd files with personally identifying information and added example files (the family of 'John Smith') instead.
---
+++ js/converter.js
@@ -0,0 +1,61 @@
+// Create an image for each row in the source database.
+//
+// usage: node converter.js
+"use strict";
+
+console.log('Starting converter.');
+
+// Configuration
+var templateFile = "template.svg";
+var csvFile = "data/example.csv";
+var svgOut = "output/svg/";
+var pngOut = "output/png/";
+var pngDimensions = [600, 600];
+
+var fs = require('fs');
+var templateData = fs.readFileSync(templateFile, 'utf8');
+processCSV(csvFile);
+
+function processCSV(csvFile) {
+  var csvData = fs.readFileSync(csvFile, 'utf8');
+	var familyArray = csvData.split(/\r?\n/);
+	var headerRow = familyArray.shift();
+	var expectedHeaderRow = "Name,Nickname,Relationship (to John Doe),Birthday,Image";
+	if (headerRow !== expectedHeaderRow) {
+		console.log("Unexpected header row. Quitting early.");
+		return;
+	}
+
+	familyArray.forEach(rowToSvg);
+}
+
+function rowToSvg(rowStr) {
+	if (!rowStr) {
+		// console.log("Skipping invalid row: [" + rowStr + "]");
+		return;
+	}
+	var rowArr = rowStr.split(',');
+	if (rowArr.length !== 5) {
+		console.log("Row of unexpected length ("+rowArr.length+"): " + rowStr);
+		return;
+	}
+
+	// ok, row is good.
+	var fileName = svgOut + rowArr[0] + ".svg";
+	fileName = fileName.replace(' ', '_');
+
+	var rowData = templateData;
+	rowData = rowData.replace('$FULL_NAME$', rowArr[0]);
+	rowData = rowData.replace('$NICKNAME$', rowArr[1]);
+	rowData = rowData.replace('$RELATIONSHIP$', rowArr[2]);
+
+	// TODO: warn about overwriting existing file
+	fs.writeFile(fileName, rowData, 'utf8');
+	console.log("wrote file: " + fileName);
+}
+
+function svgToPng() {
+  // TODO
+}
+
+console.log('done.');
